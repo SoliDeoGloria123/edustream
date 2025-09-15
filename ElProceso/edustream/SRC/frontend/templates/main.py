19040422c2f90b3b0f8b38d5d21b8995ad605a10
juan_david <juan.juand.dev@gmail.com>
2025-09-14 19:55:58 -0500

    Se agruego temporalmente lo publico

--- ElProceso/edustream/SRC/frontend/templates/main.py
+++ ElProceso/edustream/SRC/frontend/templates/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="../static"), name="static")
 templates = Jinja2Templates(directory="public")
 
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/registro")
+async def registro(request: Request):
+    return templates.TemplateResponse("registro.html", {"request": request})
+
+@app.get("/login")
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 if __name__ == "__main__":
     print("🚀 Iniciando servidor en http://localhost:8001")
